451c4d8f8be64f81040bfd88ec5514ea76fe8ee9
LHomola <lhomola83@gmail.com>
2024-04-27 22:33:42 +0100

    add basic calculation functions and operate(a,b,operator) function to call them based on operator

---
+++ main.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;    
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// call one of calculation function based on operator
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;    
+        default:
+            alert ('Invalid choice')
+            break;
+    }
+}
